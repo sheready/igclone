c893dc29501d1389aed14cafd39fba7832294a87
sheready <csstlw@gmail.com>
2020-08-11 06:05:26 +0300

    define class profileupdate,comment,userupdate,userregister forms

---
+++ igclone/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from users.models import Profile
+from . models import Comment
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email','password1','password2'] 
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username','email'] 
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image','bio']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name','body']
+
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'body':forms.Textarea(attrs={'class':'form-control'}),
+            
+        }
