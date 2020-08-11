edc2e4d7a62265ee134a156ec46ebf86096cafc0
sheready <csstlw@gmail.com>
2020-08-11 06:07:34 +0300

    define addcommentview,likeview,postdeleteview,postupdateview,postcreateview,postdetailview,postlistview and home

---
+++ igclone/views.py
@@ -0,0 +1,117 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from .models import Post, Comment
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from django.urls import reverse_lazy,reverse
+from .forms import CommentForm
+from PIL import Image
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts':Post.objects.all()
+    }
+    return render(request, 'home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+    def get_context_data(self, *args,**kwargs):
+        context = super(PostDetailView,self).get_context_data(**kwargs)
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+        context['total_likes'] = total_likes
+        return context
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post 
+    fields = ['article_image','caption']
+    template_name = 'post_form.html'
+    
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,CreateView):
+    model = Post 
+    fields = ['article_image','caption']
+    template_name = 'post_form.html'
+    
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    def save(self):
+        super().save()
+
+        img = Image.open(self.article_image.path)
+
+        if img.height > 600 or img.width > 600:
+            output_size = (600, 600)
+            img.thumbnail(output_size)
+            img.save(self.article_image.path)
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    template_name = 'post_confirm_delete.html'
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
+
+def LikeView(request,pk):
+    post = get_object_or_404(Post,id=request.POST.get('post_id'))
+    post.likes.add(request.user)
+    return HttpResponseRedirect(reverse('post-detail',args=[str(pk)]))
+
+class AddCommentView(LoginRequiredMixin,CreateView):
+    model = Comment 
+    form_class = CommentForm
+    template_name = 'add-comment.html'
+    
+    def form_valid(self, form):
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    success_url = reverse_lazy('igclone-home')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!You can now log in to InstaClone!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+
+    return render(request, 'register.html', {'form': form})
+
+
