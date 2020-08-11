37fb9962ef70d90df743fbc783fd4dc2f1ea32e5
sheready <csstlw@gmail.com>
2020-08-11 06:35:37 +0300

    add path to show profile

---
+++ socialmedia/urls.py
@@ -0,0 +1,34 @@
+"""socialmedia URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from igclone import views as igclone_views
+from users import views as users_views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('igclone.urls')),
+    path('register/',igclone_views.register,name='register'),
+    path('profile/',users_views.profile,name='profile'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name='login'),
+     path('logout/',auth_views.LogoutView.as_view(template_name = 'logout.html'),name='logout'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
