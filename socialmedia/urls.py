a8619c326ae79bb8d188fb57cae03d4142132ddf
sheready <csstlw@gmail.com>
2020-08-11 15:41:35 +0300

    add images

--- socialmedia/urls.py
+++ socialmedia/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
     path('register/',igclone_views.register,name='register'),
     path('profile/',users_views.profile,name='profile'),
     path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name='login'),
-     path('logout/',auth_views.LogoutView.as_view(template_name = 'logout.html'),name='logout'),
+    path('logout/',auth_views.LogoutView.as_view(template_name = 'logout.html'),name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
